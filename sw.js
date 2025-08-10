01080c4507ed202143764334f55de756194ff3b1
safiya-lab <safiyaca04@gmail.com>
2025-08-10 02:01:25 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+
+const CACHE_NAME = 'turbo-pt-v2-1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => {
+    if(k !== CACHE_NAME) return caches.delete(k);
+  }))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request).then(resp => {
+      const copy = resp.clone();
+      caches.open(CACHE_NAME).then(c => c.put(e.request, copy));
+      return resp;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
